6b8b742026013d97421c9ba9f5025121d69dd498
LittleTUES <118118500+LittleTUES@users.noreply.github.com>
2024-10-16 05:28:19 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
-
+var sendMail = require('../utils/config-nodemailer');
 var userModel = require("../models/user");
 
+const { createResetToken } = require('../utils/auth');
 const JWT = require('jsonwebtoken');
 const config = require("../utils/config-env");
+
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
 // });
 /**
-* @swagger
-* /users/:
-*   get:
-*     summary: Lấy danh sách người dùng
-*     responses:
-*       200:
-*         description: Thành công trả về danh sách người dùng
-*       400:
-*         description: Thất bại
-*       401:
-*         description: Unauthorized
-*       403:
-*         description: jwt expired
-*/
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Lấy danh sách tài khoản
+ *     tags: 
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Thành công trả về danh sách tài khoản
+ *       400:
+ *         description: Thất bại
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: JWT hết hạn
+ */
 router.get('/', async function (req, res) {
     try {
         const token = req.header("Authorization").split(' ')[1];
@@ -51,109 +56,187 @@ router.get('/', async function (req, res) {
  * /users/login/:
  *   post:
  *     summary: Đăng nhập tài khoản
- *     description: Đăng nhập với email và password để nhận token và refresh token.
- *     parameters:
- *       - in: body
- *         name: user
- *         description: Thông tin đăng nhập người dùng
- *         schema:
- *           type: object
- *           required:
- *             - email
- *             - password
- *           properties:
- *             email:
- *               type: string
- *             password:
- *               type: string
+ *     tags: 
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: example@example.com
+ *               password:
+ *                 type: string
+ *                 example: example
  *     responses:
  *       200:
- *         description: Thành công
+ *         description: Đăng nhập thành công
  *       400:
  *         description: Thất bại
  *       402:
- *         description: User không tồn tại
+ *         description: Tài khoản không tồn tại
  */
 router.post('/login', async function (req, res) {
-    console.log('>>>>>>>>>>>>>> In API');
-    console.log("SECRETKEY inside API: ", config.SECRETKEY); // Thêm log để kiểm tra
-
-    try {
-        const { email, password } = req.body;
-        var checkUser = await userModel.findOne({ email: email, password: password });
-        if (checkUser) {
-            console.log("User found, creating tokens...");
-            const token = JWT.sign({ id: checkUser._id }, config.SECRETKEY, { expiresIn: '30s' });
-            const refreshToken = JWT.sign({ id: checkUser._id }, config.SECRETKEY, { expiresIn: '1h' });
-            res.status(200).json({ status: true, message: "Log-in successful", token: token, refreshToken: refreshToken });
-        } else {
-            res.status(402).json({ status: false, message: "User not found" });
-        }
-    } catch (error) {
-        console.log("Error: ", error); // Log lỗi để kiểm tra
-        res.status(400).json({ status: false, message: "Log-in failed: " + error });
-    }
-});
-
-router.post('/sign-in', async function (req, res) {
-    console.log('>>>>>>>>>>>>>> In API'); // Kiểm tra log
-    console.log("SECRETKEY inside API: ", config.SECRETKEY); // Log để kiểm tra
-
     try {
         const { email, password } = req.body;
         var checkUser = await userModel.findOne({ email: email, password: password });
         if (checkUser) {
             const token = JWT.sign({ id: email }, config.SECRETKEY, { expiresIn: '30s' });
             const refreshToken = JWT.sign({ id: email }, config.SECRETKEY, { expiresIn: '1h' });
-            res.status(200).json({ status: true, message: "Đăng nhập thành công", token: token, refreshToken: refreshToken });
+            res.status(200).json({
+                status: true,
+                message: "Log-in successful",
+                token: token,
+                refreshToken: refreshToken
+            });
         } else {
-            res.status(400).json({ status: false, message: "không tìm thấy user" });
+            res.status(402).json({
+                status: false,
+                message: "User not found"
+            });
         }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(400).json({ status: false, message: "Đăng nhập thất bại" });
+        res.status(400).json({ status: false, message: "Log-in failed: " + error });
     }
 });
 
 /**
  * @swagger
- * /users/register/:
+ * /users/register:
  *   post:
  *     summary: Đăng ký tài khoản
- *     description: Đăng ký tài khoản mới với thông tin người dùng.
- *     parameters:
- *       - in: body
- *         name: user
- *         description: Thông tin đăng ký người dùng
- *         schema:
- *           type: object
- *           required:
- *             - name
- *             - email
- *             - password
- *           properties:
- *             name:
- *               type: string
- *             email:
- *               type: string
- *             password:
- *               type: string
+ *     tags: 
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: example
+ *               email:
+ *                 type: string
+ *                 example: example@example.com
+ *               password:
+ *                 type: string
+ *                 example: example
  *     responses:
  *       200:
- *         description: Thành công
+ *         description: Tạo tài khoản thành công
  *       400:
  *         description: Thất bại
  */
 router.post('/register', async function (req, res) {
     try {
-        //xử lý chức năng tương ứng với API
         const { name, email, password } = req.body;
         const account = { name, email, password };
         await userModel.create(account);
-        res.status(200).json({ status: true, message: "Create account successful" });
+        res.status(200).json({
+            status: true,
+            message: "Create account successful",
+            data: account
+        });
     } catch (err) {
         res.status(400).json({ "status": 400, message: "Failed: " + err });
     }
 });
+
+/**
+ * @swagger
+ * /users/login/forgot-password:
+ *   post:
+ *     summary: Yêu cầu đặt lại mật khẩu
+ *     tags: 
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: trinhquoctien26@gmail.com
+ *     responses:
+ *       '200':
+ *         description: Email đặt lại mật khẩu đã được gửi
+ *       '404':
+ *         description: Không tìm thấy người dùng
+ */
+router.post('/login/forgot-password', async (req, res) => {
+    const { email } = req.body;
+
+    // Kiểm tra xem email có tồn tại không
+    const user = await userModel.findOne({ email });
+    if (user) {
+        // Tạo token đặt lại mật khẩu
+        const resetToken = createResetToken(user._id);
+        // Lưu token vào cơ sở dữ liệu (tùy vào cơ sở dữ liệu của bạn)
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpires = Date.now() + 600000; // 10 phút
+        await user.save();
+        // Gửi email đặt lại mật khẩu
+        await sendMail.sendPasswordResetEmail(email, user.name, resetToken);
+        res.status(200).json({ message: 'Password reset email sent' });
+    } else {
+        return res.status(404).json({ message: 'User not found' });
+    }
+});
+
+/**
+ * @swagger
+ * /login/reset-password:
+ *   post:
+ *     summary: Đặt lại mật khẩu
+ *     tags: 
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               token:
+ *                 type: string
+ *                 example: your_reset_token
+ *               newPassword:
+ *                 type: string
+ *                 example: your_new_password
+ *     responses:
+ *       '200':
+ *         description: Password has been reset
+ *       '400':
+ *         description: Failed to reset password or token is invalid/expired
+ */
+router.post('/login/reset-password', async (req, res) => {
+    const { token, newPassword } = req.body;
+    try {
+        // Xác minh token
+        const decoded = JWT.verify(token, config.SECRETKEY);
+        const user = await userModel.findOne({ _id: decoded.id, resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+        if (user) {
+            // Cập nhật mật khẩu
+            user.password = newPassword;
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+            await user.save();
+
+            res.status(200).json({ message: 'Password has been reset' });
+        } else {
+            return res.status(400).json({ message: 'Token is invalid or has expired' });
+        }
+    } catch (err) {
+        res.status(400).json({ message: 'Failed to reset password: ' + err });
+    }
+});
+
 
 module.exports = router;
