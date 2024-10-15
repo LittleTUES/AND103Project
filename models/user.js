797189b2c8f67f66ef2294fc9ebe9fc2a2e437a4
LittleTUES <118118500+LittleTUES@users.noreply.github.com>
2024-10-16 05:28:08 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 const user = new Schema({
     id: { type: ObjectId }, //khóa chính
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
 });
 module.exports = mongoose.models.user || mongoose.model('user', user);
-// category -----> categories
