1f39223da5a4d655b7b05665b789c7ee2b47cc5d
LittleTUES <118118500+LittleTUES@users.noreply.github.com>
2024-10-16 05:28:05 +0700

    Create size.js

---
+++ models/size.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const size = new Schema({
+    id: { type: ObjectId }, //khóa chính
+    size: { type: String },
+    price: { type: Number }
+});
+module.exports = mongoose.models.size || mongoose.model('size', size);
