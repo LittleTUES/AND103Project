01ff43a8125a739f7bfa3ff5c3f554c59bac442c
LittleTUES <118118500+LittleTUES@users.noreply.github.com>
2024-10-17 13:12:40 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -81,17 +81,18 @@ router.get('/', async function (req, res) {
  */
 console.log("SECRETKKEY:", config.SECRETKEY);
 router.post('/login', async function (req, res) {
+    const secretOrPrivateKey = "TIENTQPS27928"
     try {
         const { email, password } = req.body;
         var checkUser = await userModel.findOne({ email: email, password: password });
         if (checkUser) {
             const token = JWT.sign({ id: email }, config.SECRETKEY, { expiresIn: '30s' });
-            const token2 = JWT.sign({ id: email }, "TIENTQPS27928", { expiresIn: '30s' });
+            const token2 = JWT.sign({ id: email }, secretOrPrivateKey, { expiresIn: '30s' });
             console.log('token:', token);
             console.log('token2:', token2);
             
             const refreshToken = JWT.sign({ id: email }, config.SECRETKEY, { expiresIn: '1h' });
-            const refreshToken2 = JWT.sign({ id: email }, "TIENTQPS27928", { expiresIn: '1h' });
+            const refreshToken2 = JWT.sign({ id: email }, secretOrPrivateKey, { expiresIn: '1h' });
             console.log('refreshToken:', refreshToken);
             console.log('refreshToken2:', refreshToken2);
             
